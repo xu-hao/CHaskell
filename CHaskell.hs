af784aa5139d4c42d199b96f6dbe6a68110c5296
Hao Xu <xuh@email.unc.edu>
2015-08-20 03:23:31 -0400

    updates

--- CHaskell.hs
+++ CHaskell.hs
@@ -17,19 +17,21 @@ data Expr = This
           | Var String
           | IntL Int
           | StrL String
-          | App String [Expr]
+          | App Expr [Expr]
 data Stmt = Return Expr
 data Block = Block [Stmt]
-data FuncDef = FuncDef FuncSig Block
 data Init = Init String Expr
 data Member = Field Type String
             | Ctor String String [Param] [Init] Block
-            | Method FuncDef
-data StructDef = StructDef String [Member]
-data Def = Template [Type] (Either StructDef FuncDef)
-         | NoTemplate (Either StructDef FuncDef)
+            | TemplateMember Template
+data Def = StructDef String [Member]
+         | StructFwdDecl String
          | Typedef Type String
-data Prog = Prog [Def]
+         | FuncDef FuncSig Block
+data Template = Template [Type] Def
+              | NoTemplate Def
+
+data Prog = Prog [String] [Template]
 
 instance Show Type where
     show (TVar t) = t
@@ -37,8 +39,11 @@ instance Show Type where
     show (TApp t targs) = t ++ "<" ++ intercalate "," (map show targs) ++ " >"
 instance Show Param where
     show (Param t n) = show t ++ " " ++ n
-instance Show FuncDef where
+instance Show Def where
     show (FuncDef sig block) = show sig ++ show block
+    show (StructDef n members) = "struct " ++ n ++ "{\n" ++ concatMap show members ++ "};\n"
+    show (StructFwdDecl n) = "struct " ++ n ++ ";\n"
+    show (Typedef t n) = "typedef " ++ show t ++ " " ++ n ++ ";\n"
 instance Show FuncSig where
     show (FuncSig t n ps) = show t ++ " " ++ n ++ intercalate "," (map show ps)
 instance Show Init where
@@ -48,7 +53,7 @@ instance Show Block where
 instance Show Member where
     show (Field t n) = "\t" ++ show t ++ " " ++ n ++ ";\n"
     show (Ctor mod n ps inits block) = "\t" ++ mod ++ " " ++ n ++ " (" ++ intercalate "," (map show ps) ++ "):\n\t\t" ++ intercalate "," (map show inits) ++ show block
-    show (Method fdef) = show fdef
+    show (TemplateMember template) = show template
 instance Show Expr where
     show (This) = "this"
     show (Var v) = v
@@ -56,17 +61,14 @@ instance Show Expr where
     show (Deref a) = "*(" ++ show a ++ ")"
     show (IntL i) = show i
     show (StrL s) = show s
-    show (App fn args) = fn ++ "("++ intercalate "," (map show args) ++ ")"
+    show (App fn args) = "(" ++ show fn ++ ")("++ intercalate "," (map show args) ++ ")"
 instance Show Stmt where
     show (Return v) = "return " ++ show v ++ ";\n"
-instance Show StructDef where
-    show (StructDef n members) = "struct " ++ n ++ "{\n" ++ concatMap show members ++ "};\n"
 instance Show Prog where
-    show (Prog defs) = concatMap show defs
-instance Show Def where
-    show (Template tvs def) = "template <" ++ intercalate "," (map show tvs) ++ " >\n" ++ either show show def
-    show (NoTemplate def) = either show show def
-    show (Typedef t n) = show t ++ " " ++ n ++ ";\n"
+    show (Prog preamble defs) = unlines (map ("#include " ++) preamble) ++ concatMap show defs
+instance Show Template where
+    show (Template tvs def) = "template <" ++ intercalate "," (map show tvs) ++ " >\n" ++ show def
+    show (NoTemplate def) = show def
 
 (@@) = TApp
 
@@ -75,28 +77,52 @@ variant types = "boost::variant" @@ map (("boost::recursive_wrapper" @@) . retur
 
 tranlateHaskellToCPlusPlus :: HsModule -> Prog
 tranlateHaskellToCPlusPlus (HsModule _ _ _ _ decls) =
-    Prog (concatMap tranlateHaskellDeclsToCPlusPlus decls) where
+    Prog ["<string>", "<vector>", "<utility>", "<boost/variant.hpp>"] (concatMap tranlateHaskellDeclsToCPlusPlus decls) where
+        extractConstructorName (HsRecDecl _ (HsIdent name) _) = name
+        extractConstructorName (HsConDecl _ (HsIdent name) _) = name
+
+        extractConstructorParam (name, HsUnBangedTy ty) = Param (tranlateHaskellTypeToCPlusPlus ty) (pname name)
+
+        extractFieldNameAndType (HsIdent name : _, ty) = (name, ty)
+
+        extractConstructorInit (name, _) = Init (mname name) (App (Var "std::move") [Var (pname name)])
+
         tranlateHaskellDeclsToCPlusPlus (HsDataDecl _ _ (HsIdent name) tvars constructors _) =
             let constructorNames = map extractConstructorName constructors
-                tdef = Typedef (variant (map TVar constructorNames)) name in
-                tdef : map tranlateHaskellConsDeclsToCPlusPlus constructors
-        extractConstructorName (HsRecDecl _ (HsIdent name) _) = name
+                fwddecls = map tranlateHaskellConsDeclsToFwdDeclsCPlusPlus constructors
+                tdef = NoTemplate (Typedef (variant (map TCon constructorNames)) name) in
+                fwddecls ++ tdef : map tranlateHaskellConsDeclsToCPlusPlus constructors
+
+        tranlateHaskellConsDeclsToFwdDeclsCPlusPlus (HsRecDecl _ (HsIdent name) _) =
+            NoTemplate (StructFwdDecl name)
+        tranlateHaskellConsDeclsToFwdDeclsCPlusPlus (HsConDecl _ (HsIdent name) _) =
+            NoTemplate (StructFwdDecl name)
+
         tranlateHaskellConsDeclsToCPlusPlus (HsRecDecl _ (HsIdent name) fields) =
-            let params = map extractConstructorParam fields
-                inits = map extractConstructorInit fields
+            let fields2 = map extractFieldNameAndType fields in
+                tranlateHaskellConsDeclsToCPlusPlus2 name fields2
+        tranlateHaskellConsDeclsToCPlusPlus (HsConDecl _ (HsIdent name) fields) =
+            let fields2 = zip (map (("arg" ++). show) [1..length fields]) fields in
+                tranlateHaskellConsDeclsToCPlusPlus2 name fields2
+
+        tranlateHaskellConsDeclsToCPlusPlus2 name fields2 =
+            let params = map extractConstructorParam fields2
+                inits = map extractConstructorInit fields2
                 ctor = Ctor "explicit" name params inits (Block []) in
-                NoTemplate (Left (StructDef name (ctor : map tranlateHaskellConsFieldDeclsToCPlusPlus fields)))
-        extractConstructorParam (HsIdent name : _, HsUnBangedTy ty) = Param (tranlateHaskellTypeToCPlusPlus ty) (pname name)
-        extractConstructorInit (HsIdent name : _, HsUnBangedTy ty) = Init (mname name) (Var (pname name))
-        tranlateHaskellConsFieldDeclsToCPlusPlus (HsIdent name : _, HsUnBangedTy ty) =
-            Field (tranlateHaskellTypeToCPlusPlus ty) (mname name)
+                NoTemplate (StructDef name (ctor : map tranlateHaskellRecFieldDeclsToCPlusPlus fields2))
+
+        tranlateHaskellRecFieldDeclsToCPlusPlus (i, HsUnBangedTy ty) =
+            Field (tranlateHaskellTypeToCPlusPlus ty) (mname i)
+
         tranlateHaskellTypeToCPlusPlus (HsTyVar (HsIdent name)) = TVar name
         tranlateHaskellTypeToCPlusPlus (HsTyCon (UnQual (HsIdent name))) | name == "String" = TCon "std::string"
                                                                          | name == "Int" = TCon "int"
                                                                          | otherwise = TCon name
         tranlateHaskellTypeToCPlusPlus (HsTyApp (HsTyCon (Special HsListCon)) ty) = TApp "std::vector" [tranlateHaskellTypeToCPlusPlus ty]
         tranlateHaskellTypeToCPlusPlus ty = error (show ty)
+
         pname = ("_" ++)
+
         mname = (++ "_")
 
 
