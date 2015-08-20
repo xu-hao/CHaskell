8369920bb32ddc933973162a37a4080a4671cdf2
Hao Xu <xuh@email.unc.edu>
2015-08-20 03:33:32 -0400

    updates

--- Antlr.hs
+++ Antlr.hs
@@ -1,3 +1,5 @@
 module Antlr where
 
 data Expr = Var {unVar :: String} | BinOp {left :: Expr, op :: String, right :: Expr} | ListOp {op :: String, args :: [Expr]}
+
+data ExprCon = VarCon String | BinOpCon Expr String Expr | ListOpCon String [Expr]
